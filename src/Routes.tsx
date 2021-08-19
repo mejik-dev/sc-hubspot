19f475b2b1039c39747ad14213ec260027ff8cbd
aldi kurniawan <aldikurniawan1606@gmail.com>
2021-08-20 02:52:28 +0700

    add ui send email

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
 import { UserQuery } from "./hooks/user"
 import AddOrUpdateCustomer from "./pages/customer/AddOrUpdateCustomer"
 import DetailCustomer from "./pages/customer/detail-customer/index"
+import SendEmail from "./pages/customer/detail-customer/SendEmail"
 import Customer from "./pages/customer/index"
 import Login from "./pages/login/login"
 import Register from "./pages/register/Register"
@@ -45,6 +46,9 @@ function RouterProvider(): JSX.Element {
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
 
+        <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard/customer/send-email">
+          <SendEmail />
+        </PrivateRoute>
         <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard/add-customer">
           <AddOrUpdateCustomer />
         </PrivateRoute>
@@ -57,9 +61,6 @@ function RouterProvider(): JSX.Element {
         <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard/customer">
           <DetailCustomer />
         </PrivateRoute>
-        <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard">
-          <Customer />
-        </PrivateRoute>
         <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard/add-company">
           <AddOrUpdateCompany />
         </PrivateRoute>
@@ -67,6 +68,9 @@ function RouterProvider(): JSX.Element {
           <AddOrUpdateCompany />
         </PrivateRoute>
         <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard/:currentTab">
+          <Customer />
+        </PrivateRoute>
+        <PrivateRoute autheticated={authenticated} nonAuthenticatedRedirect="/" path="/dashboard">
           <Customer />
         </PrivateRoute>
       </Switch>
