c6083be84a44aebf18dc2e104d55f409761933ba
Rizky Tanjung <kimsohyuncans@gmail.com>
2021-08-19 20:51:34 +0700

    fix: label grouped contact

--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -146,9 +146,10 @@ const Customer: React.FC = () => {
 
               return (
                 <ListComponet
+                  key={item.key}
+                  label={item.key}
                   setOpenModalFormCS={setOpenModalFormCS}
                   setDataCustomer={setDataCustomer}
-                  key={item.key}
                   dataSource={contacts}
                   handleDeleteCustomer={handleDeleteCustomer}
                 />
@@ -161,6 +162,7 @@ const Customer: React.FC = () => {
 
               return (
                 <ListComponet
+                  label={item.key}
                   setOpenModalFormCS={setOpenModalFormCS}
                   setDataCustomer={setDataCustomer}
                   key={item.key}
