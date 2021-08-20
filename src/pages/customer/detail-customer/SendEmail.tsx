67402735fe4bb54fb0182f7518a704b074338e50
aldi kurniawan <aldikurniawan1606@gmail.com>
2021-08-20 16:05:43 +0700

    add ui send email

--- src/pages/customer/detail-customer/SendEmail.tsx
+++ src/pages/customer/detail-customer/SendEmail.tsx
@@ -1,15 +1,29 @@
 import "styles/sendEmail.css"
 
-import { Input, PageHeader, Typography } from "antd"
-import SelectEmail from "components/send-email/SelectEmail"
+import { Input, PageHeader } from "antd"
+import CInputEmail from "components/send-email/CInputEmail"
 import * as React from "react"
 import { useHistory } from "react-router-dom"
 
-const { Text } = Typography
 const { TextArea } = Input
+
+interface FormValueSendEmail {
+  to?: string
+  cc?: string
+  from?: string
+  subject?: string
+  body?: string
+}
 
 const SendEmail: React.FC = () => {
   const history = useHistory()
+
+  const [values, setValues] = React.useState<FormValueSendEmail>()
+
+  const onChange = (e: React.SyntheticEvent): void => {
+    const target = e.target as HTMLInputElement
+    setValues({ ...values, [target.name]: target.value })
+  }
 
   return (
     <div>
@@ -23,17 +37,26 @@ const SendEmail: React.FC = () => {
           </div>,
         ]}
       />
-      <div className="list-data-email">
-        <Text className="label-field">To</Text>
-      </div>
-      <div className="list-data-email">
-        <Text className="label-field">Cc/Bcc</Text>
-      </div>
-      <SelectEmail label="From" value="radiegtya@gmail.com" classNameValue="select-style" />
-      <div className="list-data-email">
-        <Text className="label-field">Subject</Text>
-      </div>
-      <TextArea rows={4} placeholder="Type something brilliant..." />
+      <CInputEmail label="To" value={values?.to} classNameValue="select-style" name="to" onChange={onChange} />
+      <CInputEmail label="Cc/Bcc" value={values?.cc} classNameValue="select-style" name="cc" onChange={onChange} />
+      <CInputEmail label="From" value={values?.from} classNameValue="select-style" name="from" onChange={onChange} />
+
+      {/* <SelectEmail label="From" value="radiegtya@gmail.com" classNameValue="select-style" /> */}
+      <CInputEmail
+        label="Subject"
+        value={values?.subject}
+        classNameValue="select-style"
+        name="subject"
+        onChange={onChange}
+      />
+
+      <TextArea
+        rows={4}
+        placeholder="Type something brilliant..."
+        name="body"
+        onChange={onChange}
+        // onChange={(e) => setValues({ ...values, body: e.target.value })}
+      />
     </div>
   )
 }
